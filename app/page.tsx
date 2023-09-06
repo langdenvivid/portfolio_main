9f56b722e6142a0bde937fe098a710f89b65c34c
William Hilton <wghiltonreads@gmail.com>
2023-09-05 21:03:18 -0400

    Update container width

--- app/page.tsx
+++ app/page.tsx
@@ -34,11 +34,8 @@ export default function Home() {
 
 			<div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 			<div className="my-16 text-center animate-fade-in">
-				<h2 className="text-sm text-zinc-500 ">
-				Experienced software developer adept at crafting innovative solutions with
-				</h2>
-				<h2 className="text-sm text-zinc-500 ">
-				a commitment to clean, maintainable code and staying at the forefront of technology trends.
+				<h2 className="text-sm text-zinc-500" style={{maxWidth:"600px"}}>
+				Experienced software developer adept at crafting innovative solutions with a commitment to clean, maintainable code and staying at the forefront of technology trends.
 				</h2>
 			</div>
 		</div>
